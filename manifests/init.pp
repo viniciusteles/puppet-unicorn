a0e1f27f462edd2ac2c44cabb3e7d69934c80f83
Vinicius Teles <vinicius@improveit.com.br>
2012-10-04 16:11:38 -0300

    I needed to take into account the environment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,15 @@
 define unicorn($user_name, $home_dir, $project_name, $projects_dir, $environment='development') {
   
-  file { "/etc/init.d/unicorn_${project_name}":
+  file { "/etc/init.d/unicorn_${project_name}_${environment}":
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0755',
     content => template('unicorn/unicorn.erb'),
-    notify  => Service["unicorn_${project_name}"],
+    notify  => Service["unicorn_${project_name}_${environment}"],
   }
 
-  service { "unicorn_${project_name}":
+  service { "unicorn_${project_name}_${environment}":
     ensure     => running,
     enable     => true,
   }
